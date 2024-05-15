22aca91ee584bbed12c5bf57a8b93b386f2b27dc
kimD0ngjun <chickentasty0112@gmail.com>
2024-05-15 18:15:32 +0900

    [5.15] 백준 2206번 풀이 2차 시도 - 경로 공유 문제 및 객체 내부 요소 복사 문제

--- 김동준/벽_부수고_이동하기.py
+++ 김동준/벽_부수고_이동하기.py
@@ -1,4 +1,5 @@
 from collections import deque
+import copy
 
 
 # 풀이 최적화를 위한 함수 생성
@@ -11,15 +12,23 @@ def pathfinder(maze):
     dy = [0, 0, -1, 1]  # 좌우
 
     # 벽 + 왔던 길 : 0, 길 : 1
-    # 튜플 : (x, y, distance)
+    # 튜플 : (x, y, distance, 경로)
+    # 경로 공유가 안 되도록 지도도 슬라이싱으로 복사해서 새로운 지도 할당시키기
     queue = deque()
-    queue.append((0, 0, 1))
+    queue.append((0, 0, 1, maze))
 
     while queue:
-        x, y, distance = queue.popleft()
+        x, y, distance, prev_maze = queue.popleft()
 
-        # 팝된 상태에서 추가된 경로 방향 확인 여부
-        prev_length = len(queue)
+        # 새로운 경로 할당
+        new_maze = copy.deepcopy(prev_maze)
+
+        # 팝 지점 방문 처리
+        new_maze[x][y] = 1
+        # 팝 지점의 사방 좌표 확인용
+        four_points = []
+        # 추가 탐색 필요 여부 변수
+        need_search = True
 
         # 팝했을 때 종점(N, M)에 도착했다면
         if x == len(maze) - 1 and y == len(maze[0]) - 1:
@@ -31,48 +40,59 @@ def pathfinder(maze):
             next_x = x + dx[i]
             next_y = y + dy[i]
 
+            four_points.append((next_x, next_y))
+
             # 백트랙킹 조건
             # 맵 이탈
             if not (0 <= next_x < len(maze) and 0 <= next_y < len(maze[0])):
                 continue
 
             # 전제조건: 사방이 벽(1)이어야 함
-            if maze[next_x][next_y] == 1:
+            if new_maze[next_x][next_y] == 1:
                 continue
 
             # 방문 처리
-            if maze[next_x][next_y] == 0:
+            if new_maze[next_x][next_y] == 0:
                 # 방문했으면 다시 벽 처리
-                maze[next_x][next_y] = 1
-
+                new_maze[next_x][next_y] = 1
                 # 큐에 append 되면서 길이 변화
-                queue.append((next_x, next_y, distance + 1))
+                queue.append((next_x, next_y, distance + 1, new_maze))
+                # 추가 탐색 불필요
+                need_search = False
 
-        post_length = len(queue)
+        # 처음 탐색한 네 방향 전부 벽일때(즉 네 방향 전부 탐색이 완료됐고),
+        # 건너편 추가 탐색점이 1인지를 보는 로직이 추가로 필요
+        if need_search:
 
-        # 처음 탐색한 네 방향 전부 벽일때,
-        # 즉, 큐에 append 된 게 없을 때. 즉, prev_length가 변함이 없을 때
-        # 건너편이 1인지를 보는 로직이 추가로 필요
-        if prev_length == post_length:
+            for point in four_points:
+                point_x, point_y = point
 
-            next_dx = [2, -2, 0, 0]  # 상하
-            next_dy = [0, 0, -2, 2]  # 좌우
-
-            for i in range(4):
-                double_next_x = x + next_dx[i]
-                double_next_y = y + next_dy[i]
-
+                # 백트랙킹 조건
                 # 맵 이탈
-                if not (0 <= double_next_x < len(maze) and 0 <= double_next_y < len(maze[0])):
+                if not (0 <= point_x < len(maze) and 0 <= point_y < len(maze[0])):
                     continue
 
-                # 건너도 벽
-                if maze[double_next_x][double_next_y] == 1:
-                    continue
+                for i in range(4):
 
-                if maze[double_next_x][double_next_y] == 0:
-                    maze[double_next_x][double_next_y] = 1
-                    queue.append((double_next_x, double_next_y, distance + 2))
+                    point_next_x = point_x + dx[i]
+                    point_next_y = point_y + dy[i]
+
+                    # 백트랙킹 조건
+                    # 맵 이탈
+                    if not (0 <= point_next_x < len(maze) and 0 <= point_next_y < len(maze[0])):
+                        continue
+
+                    # 추가 탐색지점조차 벽이라면 더이상 탐색 x
+                    if new_maze[point_next_x][point_next_y] == 1:
+                        continue
+
+                    # 추가 탐색지점이 길(0)이라면
+                    if new_maze[point_next_x][point_next_y] == 0:
+                        # 방문했으면 다시 벽 처리
+                        new_maze[point_next_x][point_next_y] = 1
+
+                        # 길이는 2배
+                        queue.append((point_next_x, point_next_y, distance + 2, new_maze))
 
     # 큐가 비워질 때까지 종점의 좌표를 팝하지 못하면 그건 길이 막혔다는 뜻
     return -1
@@ -111,5 +131,8 @@ print(pathfinder(maze))
 10
 
 이런 케이스도 통과여야 되는데 불통과처리돼서 오답 처리인 것 같은데
+
+수정한 풀이 문제 확인
+경로 공유로 인한 벽뚫기 케이스 혼동 발생
 
 """
