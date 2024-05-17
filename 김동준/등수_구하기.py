38e287346f47315ade7e8de400d2feb0e1af7cda
kimD0ngjun <chickentasty0112@gmail.com>
2024-05-17 20:22:47 +0900

    [5.17] 백준 1205번 풀이 1차 시도 - 19%에서 오류

--- 김동준/등수_구하기.py
+++ 김동준/등수_구하기.py
@@ -4,19 +4,30 @@ from typing import List
 
 # 출력 함수 작성
 def solution(points: List, new_point: int):
-    # 함수 내림차순 정렬
+    """
+    우선, 정렬된 배열의 마지막 인덱스 요소와 값이 같으면 -1을 반환한다.
+    """
+    if points[len(points)-1] == new_point:
+        print(-1)
+        return
+
+    # 추가 할당 및 함수 내림차순 정렬
+    points.append(new_point)
     points.sort(reverse=True)
-    print(points)
+    # print(points)
 
     # 최적화(-1 리턴)
     # 여기서 틀렸네
-    if points.index(new_point) >= len(points) - 2:
+    """
+    또한, 정렬된 배열의 마지막 인덱스 요소의 값이 되면 -1을 반환한다.
+    """
+    if points[len(points)-1] == new_point:
         print(-1)
         return
 
     # 배열을 [요소, 등수(인덱스+1)] 형식으로 바꿈
     index_arr = list(map(lambda x: [x[1], x[0]+1], enumerate(points)))
-    print(index_arr)
+    # print(index_arr)
 
     # 투 포인터 사용해서 최적화하기
     left = 0
@@ -38,7 +49,7 @@ def solution(points: List, new_point: int):
         left = right
         right += 1
 
-    print(index_arr)
+    # print(index_arr)
     print(points.index(new_point) + 1)
 
 
@@ -52,9 +63,5 @@ P == point_range
 count, new_point, point_range = map(int, input().split())
 
 points = list(map(int, input().split()))
-
-# 추가 할당
-points.append(new_point)
-
 # 정답 출력
 solution(points, new_point)
